7a26b58c00edad15a9dae14997ac66ccdff1827f
MiraCho <miracho119@gmail.com>
2023-03-10 23:25:19 -0700

    practicing inner join exercise

--- InnerJoins.sql
+++ InnerJoins.sql
@@ -2,6 +2,7 @@
 -- Allow us to work with data from more than one related table
 -- Select the names of all the staff and the positionID they are in
 
+Select * from staff --10 
 select firstName, LastName, PositionID from Staff
 
 -- Select the names of all the staff and the position description they are in. 
@@ -13,6 +14,13 @@ select firstName, LastName, PositionID from Staff
 
     -- We need to identify how the tables are related (PK and FK) using inner join syntax
 Select FirstName, LastName, PositionDescription from Staff
+inner join position on staff.positionID=Position.PositionID 
+
+Select FirstName, LastName, PositionDescription from Position 
+inner join staff on staff.positionId = Position.PositionID
+
+    --list all the position Descriptions and the names of staff in them
+Select PositionDescription, FirstName, LastName from Staff
 inner join position on staff.positionID=Position.PositionID 
     --Oh no! We are missing one position Description (Assistant Dean) Why? because there are no staff in that position
     --Inner joins only return records if there are related records in the child tables 
@@ -39,25 +47,56 @@ Inner Join Registration on Student.StudentID = Registration.StudentID
 Inner Join Offering on Offering.OfferingCode = Registration.OfferingCode
 Inner Join Course on Course.CourseID = Offering.CourseID
 
--- Joins Exercise 1
-
+-- Joins Exercise 1 
 -- 1.	Select Student full names and the course ID's they are registered in.
+Select firstName + ' ' + lastName'StudentName', courseID from student
+inner join registration on Student.StudentID = Registration.StudentID
+inner join offering on Registration.offeringCode = offering.offeringCode
 
 -- 2.	Select the Staff full names and the Course ID’s they teach
+-- 가르치는 것은 중복됨이 없도록 한다. 
+Select distinct firstName + ' ' + lastName'StaffName', courseID from staff
+inner join offering on offering.staffID = staff.StaffID
 
 -- 3.	Select all the Club ID's and the Student full names that are in them
+select clubID, firstName + ' ' + lastName'StudentName' from activity
+inner join student on activity.studentID = student.studentID
 
 -- 4.	Select the Student full name, courseID's and marks for studentID 199899200.
+select firstName + ' ' + lastName'StudentName', courseID, mark from student 
+inner join registration on registration.studentID = student.studentID
+inner join offering on offering.offeringCode = registration.offeringCode
+where student.studentID = 199899200
 
 -- 5.	Select the Student full name, course names and marks for studentID 199899200.
+select firstName + ' ' + lastName'StudentName', courseName, mark from student 
+inner join registration on registration.studentID = student.studentID
+inner join offering on offering.offeringCode = registration.offeringCode
+inner join course on course.courseId = Offering.courseID
+where student.studentID = 199899200
 
 -- 6.	Select the CourseID, CourseNames, and the Semesters they have been taught in
+select course.courseID, CourseName, semesterCode from offering
+inner join course on course.courseId = offering.courseId
 
 -- 7.	What Staff Full Names have taught Networking 1?
+Select firstName + ' ' + lastName'StaffName' from staff
+inner join offering on offering.staffID = staff.StaffID
+inner join course on course.courseID = offering.courseID
+where courseName = 'Networking 1'
 
+-- 2개의 조건이 붙는 경우 
 -- 8.	What is the course list for student ID 199912010 in semestercode A100. Select the Students Full Name and the CourseNames.
+select firstName + ' ' + lastName'StudentName', courseName from student 
+inner join registration on registration.studentID = student.studentID
+inner join offering on offering.offeringCode = registration.offeringCode
+inner join course on course.courseId = Offering.courseID
+where semesterCode = 'A100' and student.studentID = 199912010
 
--- 9. What are the Student Names, courseID's that have Marks >80?
-
+-- 9. What are the Student Names, courseID's that have Marks > 80?
+select firstName + ' ' + lastName'StudentName', courseID, mark from student 
+inner join registration on registration.studentID = student.studentID
+inner join offering on offering.offeringCode = registration.offeringCode
+where mark > 80 
 
 
